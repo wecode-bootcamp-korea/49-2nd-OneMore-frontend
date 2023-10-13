fbc6657793884815ee06f147ab99ace4efc75e32
soonwoooo <kkshjs123@gmail.com>
2023-10-13 19:36:45 +0900

    오늘의루틴 브랜치생성

---
+++ src/components/TodayRoutine/TodayRoutine.js
@@ -0,0 +1,11 @@
+const TodayRoutine = props => {
+  const { className, alt, src, onChange, value } = props;
+
+  return (
+    <div className="todayRoutine">
+      <div></div>
+    </div>
+  );
+};
+
+export default TodayRoutine;
