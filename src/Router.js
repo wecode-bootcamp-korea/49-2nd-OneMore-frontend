ffc72e87bfc2effd01547f4f3f085512de5c8020
heejii <heejii97@naver.com>
2023-10-14 15:15:33 +0900

    [fix]리뷰반영

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import GlobalStyle from './styles/GlobalStyle';
-import GlobalFonts from './styles/fonts/fonts';
 import Main from './pages/Main/Main';
 
 function Router(props) {
   return (
     <BrowserRouter>
-      <GlobalStyle />
-      <GlobalFonts />
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
