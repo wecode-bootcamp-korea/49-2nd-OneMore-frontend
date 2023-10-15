947430def261100826d3290f1ef42995583d7c75
soonwoooo <kkshjs123@gmail.com>
2023-10-15 12:46:04 +0900

    중간 저장

--- src/components/TodayRoutine/TodayRoutine.js
+++ src/components/TodayRoutine/TodayRoutine.js
@@ -1,11 +1,19 @@
+import styled from 'styled-components';
+
 const TodayRoutine = props => {
-  const { className, alt, src, onChange, value } = props;
+  const { nickName, exerciseArea } = props;
 
   return (
-    <div className="todayRoutine">
-      <div></div>
-    </div>
+    <StyledTodayRoutine>
+      <H1>
+        {nickName}님, 오늘은 {exerciseArea} 어떠세요?
+      </H1>
+    </StyledTodayRoutine>
   );
 };
 
 export default TodayRoutine;
+
+const StyledTodayRoutine = styled.div``;
+
+const H1 = styled.h1``;
