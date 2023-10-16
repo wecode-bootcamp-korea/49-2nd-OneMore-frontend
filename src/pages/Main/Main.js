53fc48c289c1d724cfcb3b8004f6cf8bae75b2ba
soonwoooo <kkshjs123@gmail.com>
2023-10-16 15:07:36 +0900

    메인페이지 수정내용 제거

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
-import TodayRoutine from '../../components/TodayRoutine/TodayRoutine';
-
 function Main() {
-  return (
-    <div>
-      <TodayRoutine />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Main;
