e4b615c14984a888172eb9cf30c77cb5ea547e6e
Minjae Park <jayy.intheworld@gmail.com>
2023-10-17 00:10:06 +0900

    [style] 체크박스 모양, 사이즈 수정

--- src/components/CheckBox/CheckBox.js
+++ src/components/CheckBox/CheckBox.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function CheckBox({ type = 'checkbox', name, value, onChange, checked, text }) {
+function CheckBox({
+  type = 'checkbox',
+  name,
+  value,
+  onChange,
+  checked,
+  size,
+  label,
+}) {
   return (
     <CheckboxWrapper>
       <DefaultCheckBox
@@ -10,8 +18,9 @@ function CheckBox({ type = 'checkbox', name, value, onChange, checked, text }) {
         value={value}
         onChange={onChange}
         checked={checked}
+        size={size}
       />
-      <CheckboxLabel>{text}</CheckboxLabel>
+      <CheckboxLabel>{label}</CheckboxLabel>
     </CheckboxWrapper>
   );
 }
@@ -23,6 +32,7 @@ const CheckboxWrapper = styled.div`
 `;
 
 const CheckboxLabel = styled.label`
+  padding-left: 3px;
   font-size: 15px;
 `;
 
@@ -42,24 +52,27 @@ const CHECKBOX_SIZE = {
 };
 
 const DefaultCheckBox = styled.input`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   appearance: none;
   width: ${({ size }) => (size ? CHECKBOX_SIZE[size]?.width : '20px')};
   height: ${({ size }) => (size ? CHECKBOX_SIZE[size]?.height : '20px')};
-  border: 1px solid gray;
-  border-radius: 50%;
+  border: 1px solid #e0e0e0;
+  border-radius: 4px;
   outline: none;
   background-color: #ffffff;
+
   &:checked {
     border: none;
     background-color: #8bc34a;
   }
   &::after {
     content: '';
-    position: absolute;
     opacity: 0;
   }
   &:checked::after {
-    opacity: 1; /* 체크된 상태일 때 체크 마크 표시 */
+    opacity: 1;
   }
 `;
 export default CheckBox;
@@ -67,10 +80,11 @@ export default CheckBox;
 /*
 props
 
-type = [string] 입력 타입 / 기본값 : text
+type = [string] 입력 타입 / 기본값 : checkbox
 name = [string] 식별을 위한 속성
 value = [string] 입력값
 onChange = [function] 입력시 변경 이벤트
+size = [string] 체크박스의 사이즈
 label = [string] 체크박스의 라벨
 
 */
