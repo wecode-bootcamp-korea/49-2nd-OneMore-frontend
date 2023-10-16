597bd08dfa1793ca75b44dddc0ebc679849ad436
heejii <heejii97@naver.com>
2023-10-16 21:16:03 +0900

    [style] nav 컴포넌트 ui작업중

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,17 +4,35 @@ import styled from 'styled-components';
 const NavBox = styled.div`
   width: 30%;
   height: 50%;
-  background-color: blue;
+  background-color: #fff;
+  color: #333;
 
   position: fixed;
   top: 50px;
   right: 0;
 `;
 
-const NavWrap = styled.ul``;
-const NavList = styled.li``;
-const MyPage = styled.ul``;
-const MyPageList = styled.li``;
+const NavWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 20px;
+  text-indent: 10%;
+  font-size: 18px;
+`;
+const NavList = styled.span`
+  margin-bottom: 10px;
+`;
+const MyPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-indent: 20%;
+`;
+const MyPageList = styled.span`
+  margin-bottom: 10px;
+  font-size: 16px;
+`;
 
 function Nav(props) {
   return (
@@ -22,6 +40,10 @@ function Nav(props) {
       <NavWrap>
         <NavList>장바구니</NavList>
         <NavList>마이페이지</NavList>
+        <MyPage>
+          <MyPageList>내 정보 변경</MyPageList>
+          <MyPageList>식단 체크리스트</MyPageList>
+        </MyPage>
         <NavList>로그아웃</NavList>
       </NavWrap>
     </NavBox>
