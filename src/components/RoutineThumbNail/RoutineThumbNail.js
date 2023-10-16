05ce35a7b29ce60e4d2108ae414fc1cdcd194e48
soonwoooo <kkshjs123@gmail.com>
2023-10-16 10:13:23 +0900

    피드백 반영

--- src/components/RoutineThumbNail/RoutineThumbNail.js
+++ src/components/RoutineThumbNail/RoutineThumbNail.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 const RoutineThumbNail = props => {
-  const { className, alt, src } = props;
+  const { alt, src } = props;
 
   return (
     <StyledRoutineThumbNail>
-      <StyledImage className={className} src={src} alt={alt} />
+      <StyledImage src={src} alt={alt} />
     </StyledRoutineThumbNail>
   );
 };
