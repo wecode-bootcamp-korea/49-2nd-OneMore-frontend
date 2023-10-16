609d801bb9b3895125fec1922d4b777005956d7d
heejii <heejii97@naver.com>
2023-10-16 15:50:27 +0900

    [fix] layout 추가수정

--- src/styles/Layout.js
+++ src/styles/Layout.js
@@ -2,15 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 
 const LayoutBox = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   position: relative;
   background-color: #e6e6e6;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
   @media (min-width: 1024px) {
     width: 390px;
     height: 844px;
-    background-color: #e6e6e6;
   }
 `;
 
