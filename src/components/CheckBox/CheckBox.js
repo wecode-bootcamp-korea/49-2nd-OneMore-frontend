cb302467cdc4d3735098e86358cd47180df37ea3
Minjae Park <jayy.intheworld@gmail.com>
2023-10-17 15:18:46 +0900

    [style] 체크박스 체크 생성, 모양 변경

--- src/components/CheckBox/CheckBox.js
+++ src/components/CheckBox/CheckBox.js
@@ -2,33 +2,56 @@ import React from 'react';
 import styled from 'styled-components';
 
 function CheckBox({
-  type = 'checkbox',
   name,
   value,
   onChange,
   checked,
   size,
   label,
+  shape = 'square',
 }) {
   return (
     <CheckboxWrapper>
       <DefaultCheckBox
-        type={type}
+        type="checkbox"
         name={name}
         value={value}
         onChange={onChange}
         checked={checked}
         size={size}
+        shape={shape}
       />
       <CheckboxLabel>{label}</CheckboxLabel>
     </CheckboxWrapper>
   );
 }
 
+const CHECKBOX_SIZE = {
+  small: {
+    width: '17px',
+    height: '17px',
+    scale: 0.7,
+  },
+  medium: {
+    width: '24px',
+    height: '24px',
+    scale: 1,
+  },
+  large: {
+    width: '208px',
+    height: '80px',
+    scale: 4,
+  },
+};
+
+const SHAPE_BORDER_RADIUS = {
+  square: '4px',
+  round: '50%',
+};
+
 const CheckboxWrapper = styled.div`
   display: flex;
   align-items: center;
-  cursor: pointer;
 `;
 
 const CheckboxLabel = styled.label`
@@ -36,43 +59,32 @@ const CheckboxLabel = styled.label`
   font-size: 15px;
 `;
 
-const CHECKBOX_SIZE = {
-  small: {
-    width: '17px',
-    height: '17px',
-  },
-  medium: {
-    width: '24px',
-    height: '24px',
-  },
-  large: {
-    width: '208px',
-    height: '80px',
-  },
-};
-
 const DefaultCheckBox = styled.input`
   display: flex;
   align-items: center;
   justify-content: center;
+  width: ${({ size }) => CHECKBOX_SIZE[size]?.width || '20px'};
+  height: ${({ size }) => CHECKBOX_SIZE[size]?.height || '20px'};
+  border: 1px solid ${({ theme }) => theme.lightGray};
+  border-radius: ${({ shape }) => SHAPE_BORDER_RADIUS[shape] || 0};
   appearance: none;
-  width: ${({ size }) => (size ? CHECKBOX_SIZE[size]?.width : '20px')};
-  height: ${({ size }) => (size ? CHECKBOX_SIZE[size]?.height : '20px')};
-  border: 1px solid #e0e0e0;
-  border-radius: 4px;
-  outline: none;
-  background-color: #ffffff;
+  cursor: pointer;
 
   &:checked {
-    border: none;
-    background-color: #8bc34a;
-  }
-  &::after {
-    content: '';
-    opacity: 0;
-  }
-  &:checked::after {
-    opacity: 1;
+    padding-bottom: ${({ size }) => (size === 'large' ? '10px' : '2px')};
+    border-color: ${({ theme }) => theme.green};
+    background-color: ${({ theme }) => theme.green};
+
+    &:after {
+      content: '';
+      height: 6px;
+      width: 10px;
+      border: 2px solid #fff;
+      border-top: none;
+      border-right: none;
+      transform: rotate(-45deg)
+        ${({ size }) => `scale(${CHECKBOX_SIZE[size]?.scale || 1})`};
+    }
   }
 `;
 export default CheckBox;
@@ -86,5 +98,6 @@ value = [string] 입력값
 onChange = [function] 입력시 변경 이벤트
 size = [string] 체크박스의 사이즈
 label = [string] 체크박스의 라벨
+shape = [string] 체크박스의 모양 / 기본값 : square
 
 */
