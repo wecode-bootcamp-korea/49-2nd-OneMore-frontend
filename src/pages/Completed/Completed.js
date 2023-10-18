ddb12ccdef4b9d13e7d9d751945c9706ebf5a48b
heejii <heejii97@naver.com>
2023-10-19 02:48:36 +0900

    [style] complete컴포넌트 ui구현

---
+++ src/pages/Completed/Completed.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from '../../components/Button/Button';
+
+function Completed(props) {
+  return (
+    <Article>
+      <CompletedImage src="/images/2.jpg" alt="운동완료" />
+      <Button children="운동완료!" />
+    </Article>
+  );
+}
+
+const Article = styled.article`
+  width: 90%;
+  height: 75%;
+  border-radius: 25px;
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 40px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  @media (min-width: 1024px) {
+    width: 360px;
+    height: 634px;
+  }
+`;
+
+const CompletedImage = styled.img`
+  height: 80%;
+`;
+export default Completed;
