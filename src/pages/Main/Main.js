500177da1c6a885b946d0d355f403477b33d0252
soonwoooo <kkshjs123@gmail.com>
2023-10-18 11:52:21 +0900

    fix : 메인페이지 테스트 코드 제거

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,12 +1,6 @@
 import React from 'react';
 
-import TodayRoutine from '../../components/TodayRoutine/TodayRoutine';
-
 function Main(props) {
-  return (
-    <div>
-      <TodayRoutine />
-    </div>
-  );
+  return <div></div>;
 }
 export default Main;
