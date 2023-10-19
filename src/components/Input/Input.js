530c9b9bdfa7638dd68eb71bc141ec3c0f75db89
Minjae Park <jayy.intheworld@gmail.com>
2023-10-20 02:10:37 +0900

    [style] background-color 추가, border-color 수정

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -44,6 +44,8 @@ const DefaultInput = styled.input`
   font-size: 15px;
   line-height: 1.5;
   outline: none;
+  background-color: ${props =>
+    props.backgroundColor || 'white'}; // 속성에서 받아온 배경 색상을 설정
 
   &::placeholder {
     color: #e0e0e0;
@@ -54,8 +56,8 @@ const DefaultInput = styled.input`
     border: 1px solid #8bc34a;
   }
 
-  ${props => props.status === 'error' && 'border-color:black'};
-  ${props => props.status === 'done' && 'border-color:green'};
+  ${props => props.status === 'error' && 'border-color:red'};
+  ${props => props.status === 'done' && 'border-color:blue'};
 `;
 
 export default Input;
