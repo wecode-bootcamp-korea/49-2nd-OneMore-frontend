69841db419aa0005290fc62d8bce021eefc2798f
Minjae Park <jayy.intheworld@gmail.com>
2023-10-20 02:26:19 +0900

    [feat] login 수정

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -16,6 +16,10 @@ const Login = () => {
     setUserInfo({ ...userInfo, [name]: value });
   };
 
+  const handleSignUp = () => {
+    navigate('/signup');
+  };
+
   const emailRegex =
     /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
   const pwRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
@@ -23,6 +27,15 @@ const Login = () => {
   const isEmailValid = emailRegex.test(userInfo.email);
   const isPwValid = pwRegex.test(userInfo.password);
   const isValidCheck = isEmailValid && isPwValid;
+
+  // const CLIENT_ID = process.env.REACT_APP_REST_API_KEY;
+  // const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URL;
+
+  // const kakaoUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  // const handleKakaoLogin = () => {
+  //   window.location.href = kakaoUrl;
+  // };
 
   const handleLogin = e => {
     e.preventDefault();
@@ -40,21 +53,18 @@ const Login = () => {
       .then(response => {
         if (response.status === 200) {
           return response.json();
-        } else if (response.status === 400) {
+        }
+        throw new Error('communication failure');
+      })
+      .then(result => {
+        if (result.message === 'LOGIN_SUCCESS') {
+          localStorage.setItem('token', result.token);
+          localStorage.setItem('nickname', result.nickname);
+          navigate('/');
+        } else if (result.status === 400) {
           alert('아이디, 비밀번호를 확인해주세요');
         } else {
           alert('로그인 실패');
-        }
-      })
-      .then(result => {
-        if (result.token) {
-          localStorage.setItem('token', result.token);
-        }
-        if (result.nickname) {
-          localStorage.setItem('nickname', result.nickname);
-        }
-        if (result.message === 'LOGIN_SUCCESS') {
-          navigate('/');
         }
       })
       .catch(error => {
@@ -64,30 +74,73 @@ const Login = () => {
 
   return (
     <LoginWrap>
+      <LogoBox>{/* <img /> */}</LogoBox>
       <LoginInputWrap onChange={handleUserInfo}>
-        <Input name="email" size="high" placeholder="이메일을 입력해주세요" />
+        <Input
+          name="email"
+          size="high"
+          backgroundColor="#F5F6F8"
+          placeholder="이메일을 입력해주세요"
+        />
         <Input
           name="password"
           size="high"
           type="password"
+          backgroundColor="#F5F6F8"
           placeholder="비밀번호를 입력해주세요"
         />
       </LoginInputWrap>
-      <Button
-        type="submit"
-        children="로그인"
-        onClick={handleLogin}
-        disabled={!isValidCheck}
-      />
+      <LoginButtonWrap>
+        <Button
+          type="submit"
+          children="로그인"
+          onClick={handleLogin}
+          disabled={!isValidCheck}
+        />
+        <Button
+          type="submit"
+          children="회원가입"
+          backgroundColor="white"
+          color="green"
+          onClick={handleSignUp}
+        />
+        <Button
+          type="submit"
+          children="카카오로그인"
+          // onClick={handleKakaoLogin}
+        />
+      </LoginButtonWrap>
     </LoginWrap>
   );
 };
 
+const FLEX_COLUMN = `
+display: flex;
+align-items: center;
+flex-direction: column;
+gap: 5px;
+`;
+
 const LoginWrap = styled.form`
+  height: 100%;
+  border: 1px solid green;
+  background-color: white;
   padding: 15px;
 `;
+
+const LogoBox = styled.div`
+  height: 250px;
+  margin: 0 50px;
+  border: 1px solid gray;
+`;
 const LoginInputWrap = styled.div`
-  gap: 8px;
+  ${FLEX_COLUMN}
+  margin: 20px 0;
+`;
+
+const LoginButtonWrap = styled.div`
+  ${FLEX_COLUMN}
+  margin: 10px 0;
 `;
 
 export default Login;
