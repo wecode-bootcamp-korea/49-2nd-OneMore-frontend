eb53c77a5e3b1eba92e0db412d664d6b156de927
soonwoooo <kkshjs123@gmail.com>
2023-10-22 17:46:28 +0900

    완료한 운동이 0개일때 patch보내는거 막는 로직구현

--- src/pages/Main/ExerciseStart.js
+++ src/pages/Main/ExerciseStart.js
@@ -10,8 +10,6 @@ function ExerciseStart() {
   const [routineId, setRoutineId] = useState();
   const navigate = useNavigate();
 
-  console.log(completedIds, '<<');
-
   const { id } = useParams();
 
   // 머지되면 희진님 운동 완료 페이지로 이동
@@ -34,8 +32,11 @@ function ExerciseStart() {
     const completdIdSArray = localStorage.getItem('completedIds')
       ? localStorage.getItem('completedIds').split(',')
       : [];
+    if (completdIdSArray.length === 0) {
+      alert('완료한 운동이 없습니다');
+      return;
+    }
 
-    console.log(completdIdSArray, '??');
     fetch(`${BASE_API}/routines/${id}`, {
       method: 'PATCH',
       headers: {
@@ -56,8 +57,8 @@ function ExerciseStart() {
   };
 
   const getExerciseCardData = () => {
-    // fetch('/data/gyeongjae.json', {
-    fetch(`${BASE_API}/routines/${id}`, {
+    fetch('/data/gyeongjae.json', {
+      // fetch(`${BASE_API}/routines/${id}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
