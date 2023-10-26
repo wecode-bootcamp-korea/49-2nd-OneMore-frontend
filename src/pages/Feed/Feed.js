1c298ca67b160b04a3bf5022989df75c34730dc5
soonwoooo <kkshjs123@gmail.com>
2023-10-26 13:06:27 +0900

    피드 중간저장

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,66 +1,149 @@
-import { useState, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
-import Modal from '../../components/Modal/Modal';
 
 function Feed() {
   const [getFeedData, setGetFeedData] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedFeed, setSelectedFeed] = useState(null);
+  const listBox = useRef();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const page = queryParams.get('page') || '1';
+  const limit = queryParams.get('limit') || '12';
 
-  const handleModalOpen = () => {
+  const handleModalOpen = feed => {
+    setSelectedFeed(feed);
     setIsModalOpen(!isModalOpen);
   };
-  useEffect(() => {
-    fetch(`/data/feed.json`)
-      .then(res => res.json())
-      .then(data => {
-        setGetFeedData(data.data);
-      });
-  }, []);
-  console.log(getFeedData);
 
-  // "routineId": 1,
-  //   "nickName": "권순우",
-  //   "profileImage": "",
-  //   "feedImage": "13:89",
-  //   "content": "2023-10-23T03:48:12.000Z"
+  // const goToMyRoutine = id => {
+  //   navigate(`/exercise-start/${id}`);
+  // };
+
+  const getFeedDataList = () => {
+    const fetchURL = `/data/feed.json`;
+
+    fetch(fetchURL, {
+      method: 'GET',
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(result => {
+        const newFeeds = result.data;
+        setGetFeedData(prevFeedsData => [...prevFeedsData, ...newFeeds]);
+      });
+  };
+  useEffect(() => {
+    getFeedDataList();
+  }, []);
+
+  const handleScroll = () => {
+    if (
+      listBox.current.scrollHeight - listBox.current.scrollTop ===
+      listBox.current.clientHeight
+    ) {
+      getFeedDataList();
+      navigate(`/feed?page=${parseInt(page) + 1}&limit=${limit}`);
+    }
+  };
+
+  console.log(selectedFeed?.profileImage, '<<<<');
 
   return (
-    <div>
-      <BackGroundContainer>
-        <ScrollContainer>
-          <GridContainer>
-            {getFeedData.map(product => {
-              const { routineId, nickName, profileImage, feedImage, content } =
-                product;
-              return (
-                <One
-                  key={routineId}
-                  onClick={() => {
-                    isModalOpen && <Modal />;
-                  }}
-                >
-                  <FeedImage alt={nickName} src={feedImage}></FeedImage>
-                </One>
-              );
-            })}
-          </GridContainer>
-        </ScrollContainer>
-      </BackGroundContainer>
-    </div>
+    <ExerciseStartStyle>
+      <PaddingContainer ref={listBox} onScroll={handleScroll}>
+        <GridContainer>
+          {getFeedData.map(product => {
+            const { routineId, nickName, profileImage, feedImage, content } =
+              product;
+            return (
+              <One
+                key={routineId}
+                onClick={() => {
+                  handleModalOpen(product);
+                }}
+              >
+                <FeedImage alt={nickName} src={feedImage}></FeedImage>
+              </One>
+            );
+          })}
+        </GridContainer>
+      </PaddingContainer>
+
+      {isModalOpen && (
+        // <Modal feed={selectedFeed} onClose={handleModalClose} />
+        <FeedModal>
+          <TopWrapper>
+            <UserWrapper>
+              <ProfileWrapper>
+                <ProfileImage
+                  alt="프로필 사진"
+                  src={selectedFeed?.profileImage}
+                ></ProfileImage>
+              </ProfileWrapper>
+              <NickName>{selectedFeed?.nickName}</NickName>
+            </UserWrapper>
+            <ButtonWrapper>
+              <ExitButton onClick={handleModalOpen}>X</ExitButton>
+            </ButtonWrapper>
+          </TopWrapper>
+          <FeedImageWrapper>
+            <FeedImage alt="피드 이미지" src={selectedFeed?.feedImage} />
+          </FeedImageWrapper>
+          <ContentWrapper>
+            <Comment>{selectedFeed?.content}</Comment>
+            <Comment />
+            <Comment />
+          </ContentWrapper>
+        </FeedModal>
+      )}
+
+      <MakeRoutineButton>피드</MakeRoutineButton>
+    </ExerciseStartStyle>
   );
 }
 export default Feed;
 
-const BackGroundContainer = styled.div`
+const ExerciseStartStyle = styled.div``;
+
+const PaddingContainer = styled.div`
+  width: 100%;
+  padding: 0 15px 0 15px;
+  height: 580px;
+  overflow: auto;
+  margin-top: 60px;
   background-color: white;
   border-radius: 16px;
-  margin: 30px 15px 0 15px;
-  height: 75vh;
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 `;
+
+const ModalWrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 500px;
+  padding: 0 15px 0 15px;
+  top: 44%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+const MakeRoutineButton = styled.button`
+  width: 130px;
+  height: 40px;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.green};
+  display: flex;
+  justify-content: center;
+  color: white;
+  align-items: center;
+  position: absolute;
+  right: 25px;
+  bottom: 20px;
+  top: calc(100% - 60px);
+  right: 25px;
+`;
+
 const ScrollContainer = styled.div`
   width: 100%;
   height: 600px;
@@ -72,33 +155,87 @@ const GridContainer = styled.div`
   justify-content: center;
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: repeat(4, 1fr);
-  height: 480px;
   border-radius: 16px;
   position: relative;
-  top: 50px;
 `;
 
-// const Container = styled.div`
-//   width: 100%;
-//   background-color: pink;
-//   height: 223px;
-//   border-radius: 16px;
-
-//   position: relative;
-// `;
-
 const One = styled.div`
-  /* border-bottom: 1px solid blue;
-  border-right: 1px solid blue; */
   border: 0.25px solid gray;
   display: flex;
   justify-content: center;
   align-items: center;
   border-radius: 2px;
-  overflow: auto;
 `;
 
 const FeedImage = styled.img`
-  height: 110px;
-  width: 110px;
+  height: 100%;
+  width: 100%;
+`;
+
+const FeedModal = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  height: 500px;
+  width: 80%;
+  box-shadow: 5px 5px 5px 1px gray;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const TopWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 30px 20px 0 20px;
+`;
+
+const ProfileWrapper = styled.div`
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+`;
+const ProfileImage = styled.img`
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+`;
+const UserWrapper = styled.div`
+  display: flex;
+  align-items: flex-end;
+`;
+const NickName = styled.p`
+  font-weight: 700;
+  font-size: 18px;
+  text-align: center;
+  line-height: 1.2;
+  margin-left: 10px;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+`;
+const ExitButton = styled.button`
+  font-size: 25px;
+`;
+
+const FeedImageWrapper = styled.div`
+  margin-top: 10px;
+  height: 40%;
+  padding: 5px;
+  border-bottom: 0.25px solid gray;
+`;
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: auto;
+  padding: 15px 15px 0px 15px;
+`;
+
+const Comment = styled.p`
+  font-size: 16px;
 `;
